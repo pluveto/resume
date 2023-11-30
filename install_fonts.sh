daf913f5844c8fe0a335aad61b637575307f2797
pluveto <i@pluvet.com>
2023-11-30 21:46:14 +0800

    update

--- install_fonts.sh
+++ install_fonts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-font_url="https://github.com/adobe-fonts/source-han-serif/raw/release/SubsetOTF/SourceHanSerifSC.zip"
+font_url="https://github.com/adobe-fonts/source-han-serif/releases/download/2.002R/09_SourceHanSerifSC.zip"
 font_dir="$HOME/Library/Fonts/SourceHanSerif"
 mkdir -p "$font_dir"
 curl -L "$font_url" -o "/tmp/SourceHanSerif.zip"
