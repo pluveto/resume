af646a44f0b4d891fcde788a2d4556d801ef51ff
pluveto <i@pluvet.com>
2024-01-06 10:50:56 +0800

    chore: update

---
+++ serve.py
@@ -0,0 +1,14 @@
+# serve current directory on port 8000
+# python3 serve.py
+
+import http.server
+import socketserver
+
+PORT = 8000
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
+    
